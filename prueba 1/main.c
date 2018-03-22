3244e79bccf1f0e5ddc2d9b387315eed4c0e97dd
mariela <36265002+mariela27@users.noreply.github.com>
2018-03-22 10:58:50 -0300

    Add files via upload

---
+++ prueba 1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    int  (%d) (numero entero)
+    float (%f) (numeros con coma)
+    char (%c) (caracter "a" "5")
+
+
+    */
+    int numero1;
+    numero1=88;
+    float numero2;
+    numero2=889.56669;
+    printf("el numero es: %d y el numero flotante es:%.0f" , numero1, numero2);
+    return 0;
+}
