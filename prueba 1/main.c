6edf3b862df186276621056465764fbd26ad9dee
mariela <36265002+mariela27@users.noreply.github.com>
2018-03-22 12:15:04 -0300

    Add files via upload

--- prueba 1/main.c
+++ prueba 1/main.c
@@ -10,10 +10,26 @@ int main()
 
 
     */
-    int numero1;
-    numero1=88;
+    int numero1=9 ;
     float numero2;
-    numero2=889.56669;
+    char letra;
+    float suma;
+    printf("ingrese un numero entero: ");
+    scanf("%d", &numero1);
+    printf("ingrese un numero con coma:");
+    scanf("%f", &numero2);
+    printf("ingrese la letra: ");
+    /*fflush(stdin);
+    scanf("%c", &letra);*/
+
+    //getche()/getch()
+
+    letra= getch();
+
+    suma=numero1+numero2;
+
     printf("el numero es: %d y el numero flotante es:%.0f" , numero1, numero2);
+    printf("\nla suma es: %.2f", suma );
+    printf("\nla letra es: %c", letra );
     return 0;
 }
