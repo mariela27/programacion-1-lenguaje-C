583e1c96de39c4c3e94046a484e4d6fd27a717a6
mariela <36265002+mariela27@users.noreply.github.com>
2018-04-05 12:10:54 -0300

    revisar y hacer numeros pares y enteros

---
+++ clase 3/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+void saludar();
+
+int main()
+{
+    saludar();
+
+    return 0;
+}
+void saludar()
+{
+    printf("hola mundo!\n");
+}
+
+
+int sumar(int numeroUno,int numeroDos);
+int main()
+{
+    int numero;
+    numero=sumar(3,3);
+    printf("%d",numero);
+    return 0;
+}
+int sumar(int numeroUno,int numeroDos)
+{
+    int resultado=0;
+    resultado=numeroUno+numeroDos;
+    return resultado;
+}
+
+float divisionEnteros(float numeroUno, float numeroDos);
+int main()
+{
+    float numero;
+    numero=divisionEnteros(10,3);
+    printf("%f", numero);
+    return 0;
+}
+float divisionEnteros(float numeroUno, float numeroDos)
+{
+    float resultado=0;
+    resultado=numeroUno/numeroDos;
+    return resultado;
+}
+*/
+();
+int main()
+{
+   int sePudo;
+   sePudo=pedirNumerosPar();
+   printf("d", pedirNumeros);
+   if(sePudo==0)
+   {
+       printf("ok");
+   }
+   else
+   {
+       printf("mal");
+   }
+}
+
